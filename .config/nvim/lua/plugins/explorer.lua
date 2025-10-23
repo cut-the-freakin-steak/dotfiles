846b7c1710cc39c271b622797461319bc735ffc6
cut-the-freakin-steak <calvaradolopez@proton.me>
2025-10-22 20:52:26 -0600

    added some .gitconfig stuff

--- .config/nvim/lua/plugins/explorer.lua
+++ .config/nvim/lua/plugins/explorer.lua
@@ -1,31 +1,5 @@
 return {
 	"folke/snacks.nvim",
-	opts = {
-		explorer = {
-			hidden = false, -- don’t show dotfiles
-			ignored = false, -- hide gitignored files
-			exclude = {
-				".git",
-				"__pycache__",
-				"node_modules",
-				"*.tmp",
-				"*.bak",
-				".DS_Store",
-			},
-		},
-		picker = {
-			sources = {
-				explorer = {
-					hidden = false,
-					ignored = false,
-				},
-				files = {
-					hidden = false,
-					ignored = false,
-				},
-			},
-		},
-		hidden = false,
-		ignored = true,
-	},
+	---@type snacks.Config
+	opts = {},
 }
