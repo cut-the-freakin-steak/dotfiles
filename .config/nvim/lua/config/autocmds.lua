95ed920604199dd01a34540a87560e50c4fdb262
cut-the-freakin-steak <calvaradolopez@proton.me>
2026-02-24 14:29:43 -0700

    dude yayy

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("ExitPre", {
+	pattern = "*",
+	callback = function(event)
+		for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+			if vim.api.nvim_buf_get_option(buf, "buftype") == "terminal" then
+				vim.api.nvim_buf_delete(buf, { force = true })
+			end
+		end
+	end,
+})
